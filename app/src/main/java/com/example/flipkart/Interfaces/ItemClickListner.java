1e0e7c8cf71fb3f467a7225bf8705db00de8f6d0
Udaya Kiran Rokalla <urokalla@corp.miraclesoft.com>
2019-11-21 22:40:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flipkart/Interfaces/ItemClickListner.java
@@ -0,0 +1,7 @@
+package com.example.flipkart.Interfaces;
+
+import android.view.View;
+
+public interface ItemClickListner {
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/example/flipkart/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.flipkart.Prevalent;
+
+import com.example.flipkart.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserphoneKey = "Userphone";
+    public static final String UserPasswordKey = "UserPassword";
+}
